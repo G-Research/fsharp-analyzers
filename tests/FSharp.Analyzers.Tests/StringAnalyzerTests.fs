ce3c91b078ecc87ca19090720556bd4da320e27b
dawe <dawedawe@posteo.de>
2023-09-26 15:20:44 +0200

    Factor out common code (#5)
    * factor out common testing code
    
    * Move TAST handling to it's own module and start the ground work to have different handlers for the various parts of the TAST an analyzer might want to handle.
    
    * "PackageReference Update" -> "PackageReference Include" for FSharp.Core
    
    * Revert ""PackageReference Update" -> "PackageReference Include" for FSharp.Core"
    
    This reverts commit 4f3e2b19787c2bd46390169cbe277004bc434e00.

--- tests/FSharp.Analyzers.Tests/StringAnalyzerTests.fs
+++ tests/FSharp.Analyzers.Tests/StringAnalyzerTests.fs
@@ -1,110 +1,76 @@
-module ``G-Research``.FSharp.Analyzers.Tests
+namespace ``G-Research``.FSharp.Analyzers.Tests
 
-open System.Collections
-open System.IO
-open System.Threading.Tasks
-open FSharp.Compiler.CodeAnalysis
-open NUnit.Framework
-open FSharp.Analyzers.SDK
-open FSharp.Analyzers.SDK.Testing
+module StringAnalyzerTests =
 
-let shouldUpdateBaseline () =
-    System.Environment.GetEnvironmentVariable "TEST_UPDATE_BSL"
-    |> Option.ofObj
-    |> Option.map (fun v -> v.Trim () = "1")
-    |> Option.defaultValue false
+    open System.Collections
+    open System.IO
+    open FSharp.Compiler.CodeAnalysis
+    open NUnit.Framework
+    open FSharp.Analyzers.SDK
+    open FSharp.Analyzers.SDK.Testing
+    open ``G-Research``.FSharp.Analyzers
+    open Testing
 
-let assertExpected sourceFile messages =
-    task {
-        let actualContents =
-            messages
-            |> List.map (fun (m : Message) ->
-                $"%s{m.Code} | %A{m.Severity} | (%i{m.Range.StartLine},%i{m.Range.StartColumn} - %i{m.Range.EndLine},%i{m.Range.EndColumn}) | %s{m.Message}"
-            )
-            |> String.concat "\n"
-            |> fun contents -> System.String.Concat (contents, "\n")
+    let mutable projectOptions : FSharpProjectOptions = FSharpProjectOptions.zero
 
-        let expectedFile = $"%s{sourceFile}.expected"
-        let actualFile = $"%s{sourceFile}.actual"
+    [<SetUp>]
+    let Setup () =
+        task {
+            let! options = mkOptionsFromProject "net6.0" []
+            projectOptions <- options
+        }
 
-        let! expectedContents =
-            if File.Exists expectedFile then
-                File.ReadAllTextAsync expectedFile
-            else
-                Task.FromResult "No baseline was found"
+    type TestCases() =
+        static member DataFolder = Path.Combine (__SOURCE_DIRECTORY__, "..", "data")
 
-        let areEqual = expectedContents = actualContents
+        interface IEnumerable with
+            member _.GetEnumerator () : IEnumerator =
+                let endsWithTests = Path.Combine (TestCases.DataFolder, "string", "endswith")
 
-        if shouldUpdateBaseline () then
-            do! File.WriteAllTextAsync (expectedFile, actualContents)
-        elif not areEqual then
-            do! File.WriteAllTextAsync (actualFile, actualContents)
-        elif File.Exists actualFile then
-            File.Delete actualFile
+                Directory.EnumerateFiles (endsWithTests, "*.fs")
+                |> Seq.map (fun f ->
+                    let fileName = Path.GetRelativePath (TestCases.DataFolder, f)
+                    [| fileName :> obj |]
+                )
+                |> fun s -> s.GetEnumerator ()
 
-        Assert.AreEqual (expectedContents, actualContents)
-    }
+    [<TestCaseSource(typeof<TestCases>)>]
+    let EndsWithTests (fileName : string) =
+        task {
+            let dataFolder = TestCases.DataFolder
+            let fileName = Path.Combine (dataFolder, fileName)
 
-let mutable projectOptions : FSharpProjectOptions = FSharpProjectOptions.zero
+            let! messages =
+                File.ReadAllText fileName
+                |> getContext projectOptions
+                |> StringAnalyzers.endsWithAnalyzer
 
-[<SetUp>]
-let Setup () =
-    task {
-        let! options = mkOptionsFromProject "net6.0" []
-        projectOptions <- options
-    }
+            do! assertExpected fileName messages
+        }
 
-type TestCases() =
-    static member DataFolder = Path.Combine (__SOURCE_DIRECTORY__, "..", "data")
+    type NegativeTestCases() =
 
-    interface IEnumerable with
-        member _.GetEnumerator () : IEnumerator =
-            let endsWithTests = Path.Combine (TestCases.DataFolder, "string", "endswith")
+        interface IEnumerable with
+            member _.GetEnumerator () : IEnumerator =
+                let endsWithTests =
+                    Path.Combine (TestCases.DataFolder, "string", "endswith", "negative")
 
-            Directory.EnumerateFiles (endsWithTests, "*.fs")
-            |> Seq.map (fun f ->
-                let fileName = Path.GetRelativePath (TestCases.DataFolder, f)
-                [| fileName :> obj |]
-            )
-            |> fun s -> s.GetEnumerator ()
+                Directory.EnumerateFiles (endsWithTests, "*.fs")
+                |> Seq.map (fun f ->
+                    let fileName = Path.GetRelativePath (TestCases.DataFolder, f)
+                    [| fileName :> obj |]
+                )
+                |> fun s -> s.GetEnumerator ()
 
-[<TestCaseSource(typeof<TestCases>)>]
-let EndsWithTests (fileName : string) =
-    task {
-        let dataFolder = TestCases.DataFolder
-        let fileName = Path.Combine (dataFolder, fileName)
+    [<TestCaseSource(typeof<NegativeTestCases>)>]
+    let NegativeEndsWithTests (fileName : string) =
+        task {
+            let fileName = Path.Combine (TestCases.DataFolder, fileName)
 
-        let! messages =
-            File.ReadAllText fileName
-            |> getContext projectOptions
-            |> StringAnalyzers.endsWithAnalyzer
+            let! messages =
+                File.ReadAllText fileName
+                |> getContext projectOptions
+                |> StringAnalyzers.endsWithAnalyzer
 
-        do! assertExpected fileName messages
-    }
-
-type NegativeTestCases() =
-
-    interface IEnumerable with
-        member _.GetEnumerator () : IEnumerator =
-            let endsWithTests =
-                Path.Combine (TestCases.DataFolder, "string", "endswith", "negative")
-
-            Directory.EnumerateFiles (endsWithTests, "*.fs")
-            |> Seq.map (fun f ->
-                let fileName = Path.GetRelativePath (TestCases.DataFolder, f)
-                [| fileName :> obj |]
-            )
-            |> fun s -> s.GetEnumerator ()
-
-[<TestCaseSource(typeof<NegativeTestCases>)>]
-let NegativeEndsWithTests (fileName : string) =
-    task {
-        let fileName = Path.Combine (TestCases.DataFolder, fileName)
-
-        let! messages =
-            File.ReadAllText fileName
-            |> getContext projectOptions
-            |> StringAnalyzers.endsWithAnalyzer
-
-        Assert.IsEmpty messages
-    }
+            Assert.IsEmpty messages
+        }
---
+++ tests/FSharp.Analyzers.Tests/Testing.fs
@@ -0,0 +1,46 @@
+﻿namespace ``G-Research``.FSharp.Analyzers.Tests
+
+module Testing =
+
+    open System.IO
+    open System.Threading.Tasks
+    open NUnit.Framework
+    open FSharp.Analyzers.SDK
+    open FSharp.Analyzers.SDK.Testing
+
+    let shouldUpdateBaseline () =
+        System.Environment.GetEnvironmentVariable "TEST_UPDATE_BSL"
+        |> Option.ofObj
+        |> Option.map (fun v -> v.Trim () = "1")
+        |> Option.defaultValue false
+
+    let assertExpected sourceFile messages =
+        task {
+            let actualContents =
+                messages
+                |> List.map (fun (m : Message) ->
+                    $"%s{m.Code} | %A{m.Severity} | (%i{m.Range.StartLine},%i{m.Range.StartColumn} - %i{m.Range.EndLine},%i{m.Range.EndColumn}) | %s{m.Message}"
+                )
+                |> String.concat "\n"
+                |> fun contents -> System.String.Concat (contents, "\n")
+
+            let expectedFile = $"%s{sourceFile}.expected"
+            let actualFile = $"%s{sourceFile}.actual"
+
+            let! expectedContents =
+                if File.Exists expectedFile then
+                    File.ReadAllTextAsync expectedFile
+                else
+                    Task.FromResult "No baseline was found"
+
+            let areEqual = expectedContents = actualContents
+
+            if shouldUpdateBaseline () then
+                do! File.WriteAllTextAsync (expectedFile, actualContents)
+            elif not areEqual then
+                do! File.WriteAllTextAsync (actualFile, actualContents)
+            elif File.Exists actualFile then
+                File.Delete actualFile
+
+            Assert.AreEqual (expectedContents, actualContents)
+        }
