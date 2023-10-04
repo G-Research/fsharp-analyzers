ee2d8ebd76408b447458326e0f77c6cbf3d9130e
dawe <dawedawe@posteo.de>
2023-10-04 12:20:17 +0200

    adjust namespaces

--- tests/FSharp.Analyzers.Tests/PartialAppAnalyzerTests.fs
+++ tests/FSharp.Analyzers.Tests/PartialAppAnalyzerTests.fs
@@ -1,4 +1,4 @@
-﻿namespace ``G-Research``.FSharp.Analyzers.Tests
+﻿namespace GR.FSharp.Analyzers.Tests
 
 module PartialAppAnalyzerTests =
 
@@ -7,8 +7,8 @@ module PartialAppAnalyzerTests =
     open NUnit.Framework
     open FSharp.Compiler.CodeAnalysis
     open FSharp.Analyzers.SDK.Testing
-    open ``G-Research``.FSharp.Analyzers
-    open Testing
+    open GR.FSharp.Analyzers
+    open GR.FSharp.Analyzers.Tests.Common
 
     let mutable projectOptions : FSharpProjectOptions = FSharpProjectOptions.zero
 
