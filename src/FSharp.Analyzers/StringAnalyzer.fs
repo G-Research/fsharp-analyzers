a7e6bc448398ab2f29ca55d23fcf18fe9cf49e8d
nojaf <florian.verdonck@outlook.com>
2023-10-05 13:14:37 +0200

    Update string analyzers to use the typed tree.

--- src/FSharp.Analyzers/StringAnalyzer.fs
+++ src/FSharp.Analyzers/StringAnalyzer.fs
@@ -1,10 +1,9 @@
 ﻿module GR.FSharp.Analyzers.StringAnalyzer
 
 open FSharp.Analyzers.SDK
-open FSharp.Compiler.CodeAnalysis
 open FSharp.Compiler.Symbols
-open FSharp.Compiler.Syntax
 open FSharp.Compiler.Text
+open GR.FSharp.Analyzers.TASTCollecting
 
 [<Literal>]
 let StringEndsWithCode = "GRA-STRING-001"
@@ -15,178 +14,112 @@ let StringStartsWithCode = "GRA-STRING-002"
 [<Literal>]
 let StringIndexOfCode = "GRA-STRING-003"
 
-let (|SingleNameInSynLongIdent|_|) name (lid : SynLongIdent) =
-    match lid with
-    | SynLongIdent (id = [ ident ]) when ident.idText = name -> Some ident.idRange
-    | _ -> None
+let (|StringExpr|_|) (e : FSharpExpr) =
+    if e.Type.ErasedType.BasicQualifiedName = "System.String" then
+        Some ()
+    else
+        None
 
-let (|SynLongIdentEndsWith|_|) name (lid : SynLongIdent) =
-    List.tryLast lid.LongIdent
-    |> Option.bind (fun ident -> if ident.idText = name then Some ident.idRange else None)
-
-let rec (|SingleStringArgumentExpr|_|) =
-    function
-    // Strip parentheses
-    | SynExpr.Paren (expr = SingleStringArgumentExpr) -> Some ()
-    // Only allow ""
-    | SynExpr.Const (constant = constant) ->
-        match constant with
-        | SynConst.String _ -> Some ()
-        | _ -> None
-    // Don't allow tuples and any other obvious non value expression
-    | SynExpr.Tuple _
-    | SynExpr.Lambda _
-    | SynExpr.MatchLambda _
-    | SynExpr.MatchBang _
-    | SynExpr.LetOrUseBang _
-    | SynExpr.AnonRecd _
-    | SynExpr.ArrayOrList _
-    | SynExpr.ArrayOrListComputed _
-    | SynExpr.Assert _
-    | SynExpr.DoBang _
-    | SynExpr.DotSet _
-    | SynExpr.For _
-    | SynExpr.ForEach _
-    | SynExpr.Lazy _
-    | SynExpr.Record _
-    | SynExpr.Set _
-    | SynExpr.While _
-    | SynExpr.YieldOrReturn _
-    | SynExpr.YieldOrReturnFrom _ -> None
-    // Allow pretty much any expression
-    | _ -> Some ()
-
-let findAllInvocations (parameterPredicate : SynExpr -> bool) (functionName : string) (ast : ParsedInput) : range list =
-    let collector = ResizeArray<range> ()
-
-    let walker =
-        { new SyntaxCollectorBase() with
-            override _.WalkExpr (expr : SynExpr) =
-                match expr with
-                // "".FunctionName arg
-                | SynExpr.App (
-                    isInfix = false
-                    funcExpr = SynExpr.DotGet (longDotId = SingleNameInSynLongIdent functionName mFunctionName)
-                    argExpr = argExpr)
-
-                // w.FunctionName arg
-                | SynExpr.App (
-                    funcExpr = SynExpr.LongIdent (longDotId = SynLongIdentEndsWith functionName mFunctionName)
-                    argExpr = argExpr) when parameterPredicate argExpr -> collector.Add mFunctionName
-
-                | _ -> ()
-        }
-
-    walkAst walker ast
-
-    collector |> Seq.toList
+let (|IntExpr|_|) (e : FSharpExpr) =
+    if e.Type.ErasedType.BasicQualifiedName = "System.Int32" then
+        Some ()
+    else
+        None
 
 let invalidStringFunctionUseAnalyzer
     functionName
     code
     message
     severity
-    (sourceText : ISourceText)
-    (untypedTree : ParsedInput)
-    (checkFileResults : FSharpCheckFileResults)
-    (unTypedArgumentPredicate : SynExpr -> bool)
-    (typedArgumentPredicate : FSharpMemberOrFunctionOrValue -> bool)
+    (typedTree : FSharpImplementationFileContents)
+    (typedArgumentPredicate : FSharpExpr list -> bool)
     =
-    async {
-        let invocations =
-            findAllInvocations unTypedArgumentPredicate functionName untypedTree
+    let invocations = ResizeArray<range> ()
 
-        return
-            invocations
-            |> List.choose (fun mEndsWith ->
-                let lineText = sourceText.GetLineString (mEndsWith.EndLine - 1)
+    let handler : Handler =
+        Handler.CallHandler (fun (m : range) (mfv : FSharpMemberOrFunctionOrValue) (args : FSharpExpr list) ->
+            if
+                mfv.Assembly.SimpleName = "System.Runtime"
+                && mfv.FullName = $"System.String.{functionName}"
+                && typedArgumentPredicate args
+            then
+                invocations.Add m
+        )
 
-                let symbolUseOpt =
-                    checkFileResults.GetSymbolUseAtLocation (
-                        mEndsWith.EndLine,
-                        mEndsWith.EndColumn,
-                        lineText,
-                        [ "EndsWith" ]
-                    )
+    for decl in typedTree.Declarations do
+        visitDeclaration handler decl
 
-                symbolUseOpt
-                |> Option.bind (fun symbolUse ->
-                    match symbolUse.Symbol with
-                    | :? FSharpMemberOrFunctionOrValue as mfv ->
-                        if mfv.Assembly.SimpleName <> "netstandard" then
-                            None
-                        elif not (mfv.FullName = $"System.String.%s{functionName}") then
-                            None
-                        elif not (typedArgumentPredicate mfv) then
-                            None
-                        else
-                            Some mEndsWith
-                    | _ -> None
-                )
-            )
-            |> List.map (fun mFunctionName ->
-                {
-                    Type = $"String.{functionName} analyzer"
-                    Message = message
-                    Code = code
-                    Severity = severity
-                    Range = mFunctionName
-                    Fixes = []
-                }
-            )
-    }
-
-let hasMatchingSignature (expectedSignature : string) (mfv : FSharpMemberOrFunctionOrValue) : bool =
-    let rec visit (fsharpType : FSharpType) =
-        if fsharpType.GenericArguments.Count = 0 then
-            fsharpType.ErasedType.BasicQualifiedName
-        else
-            fsharpType.GenericArguments |> Seq.map visit |> String.concat " -> "
-
-    let actualSignature = visit mfv.FullType
-    actualSignature = expectedSignature
+    invocations
+    |> Seq.map (fun mFunctionName ->
+        {
+            Type = $"String.{functionName} analyzer"
+            Message = message
+            Code = code
+            Severity = severity
+            Range = mFunctionName
+            Fixes = []
+        }
+    )
+    |> Seq.toList
 
 [<CliAnalyzer "String.EndsWith Analyzer">]
 let endsWithAnalyzer (ctx : CliContext) : Async<Message list> =
-    invalidStringFunctionUseAnalyzer
-        "EndsWith"
-        StringEndsWithCode
-        "The usage of String.EndsWith with a single string argument is discouraged. Signal your intention explicitly by calling an overload."
-        Warning
-        ctx.SourceText
-        ctx.ParseFileResults.ParseTree
-        ctx.CheckFileResults
-        (function
-        | SingleStringArgumentExpr _ -> true
-        | _ -> false)
-        (hasMatchingSignature "System.String -> System.Boolean")
+    async {
+        match ctx.TypedTree with
+        | None -> return List.empty
+        | Some typedTree ->
+
+        return
+            invalidStringFunctionUseAnalyzer
+                "EndsWith"
+                StringEndsWithCode
+                "The usage of String.EndsWith with a single string argument is discouraged. Signal your intention explicitly by calling an overload."
+                Warning
+                typedTree
+                (fun (args : FSharpExpr list) ->
+                    match args with
+                    | [ StringExpr ] -> true
+                    | _ -> false
+                )
+    }
 
 [<CliAnalyzer "String.StartsWith Analyzer">]
 let startsWithAnalyzer (ctx : CliContext) : Async<Message list> =
-    invalidStringFunctionUseAnalyzer
-        "StartsWith"
-        StringStartsWithCode
-        "The usage of String.StartsWith with a single string argument is discouraged. Signal your intention explicitly by calling an overload."
-        Warning
-        ctx.SourceText
-        ctx.ParseFileResults.ParseTree
-        ctx.CheckFileResults
-        (function
-        | SingleStringArgumentExpr _ -> true
-        | _ -> false)
-        (hasMatchingSignature "System.String -> System.Boolean")
+    async {
+        match ctx.TypedTree with
+        | None -> return List.empty
+        | Some typedTree ->
+            return
+                invalidStringFunctionUseAnalyzer
+                    "StartsWith"
+                    StringStartsWithCode
+                    "The usage of String.StartsWith with a single string argument is discouraged. Signal your intention explicitly by calling an overload."
+                    Warning
+                    typedTree
+                    (fun (args : FSharpExpr list) ->
+                        match args with
+                        | [ StringExpr ] -> true
+                        | _ -> false
+                    )
+    }
 
 [<CliAnalyzer "String.IndexOf Analyzer">]
 let indexOfAnalyzer (ctx : CliContext) : Async<Message list> =
-    invalidStringFunctionUseAnalyzer
-        "IndexOf"
-        StringIndexOfCode
-        "The usage of String.IndexOf with a single string argument is discouraged. Signal your intention explicitly by calling an overload."
-        Warning
-        ctx.SourceText
-        ctx.ParseFileResults.ParseTree
-        ctx.CheckFileResults
-        (function
-        | SingleStringArgumentExpr _ -> true
-        | _ -> false)
-        (hasMatchingSignature "System.String -> System.Int32")
+    async {
+        match ctx.TypedTree with
+        | None -> return List.empty
+        | Some typedTree ->
+
+        return
+            invalidStringFunctionUseAnalyzer
+                "IndexOf"
+                StringIndexOfCode
+                "The usage of String.IndexOf with a single string argument is discouraged. Signal your intention explicitly by calling an overload."
+                Warning
+                typedTree
+                (fun args ->
+                    match args with
+                    | [ StringExpr ] -> true
+                    | _ -> false
+                )
+    }
