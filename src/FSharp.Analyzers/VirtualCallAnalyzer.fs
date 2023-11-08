bab3dfd914440b8d1308c94c63b2041ddbd053b6
dawe <dawedawe@posteo.de>
2023-11-08 12:41:42 +0100

    refactor

--- src/FSharp.Analyzers/VirtualCallAnalyzer.fs
+++ src/FSharp.Analyzers/VirtualCallAnalyzer.fs
@@ -23,114 +23,116 @@ let (|CoerceToSeq|_|) (includeFromSet : bool) (expr : FSharpExpr) =
         | _ -> None
     | _ -> None
 
-[<CliAnalyzer>]
+let seqFuncsWithEquivalentsInAllCollections =
+    set
+        [
+            "Microsoft.FSharp.Collections.Seq.isEmpty"
+            "Microsoft.FSharp.Collections.Seq.length" // ~ Set.count
+            "Microsoft.FSharp.Collections.Seq.max" // ~ Set.maxElement
+            "Microsoft.FSharp.Collections.Seq.min" // ~ Set.minElement
+            "Microsoft.FSharp.Collections.Seq.contains"
+            "Microsoft.FSharp.Collections.Seq.exists"
+            "Microsoft.FSharp.Collections.Seq.filter"
+            "Microsoft.FSharp.Collections.Seq.foldBack"
+            "Microsoft.FSharp.Collections.Seq.forall"
+            "Microsoft.FSharp.Collections.Seq.iter"
+            "Microsoft.FSharp.Collections.Seq.map"
+            "Microsoft.FSharp.Collections.Seq.fold"
+            "Microsoft.FSharp.Collections.Seq.except" // ~ Set.difference
+        ]
+
+let seqFuncsWithEquivalentsInArrayAndList =
+    set
+        [
+            "Microsoft.FSharp.Collections.Seq.average"
+            "Microsoft.FSharp.Collections.Seq.distinct"
+            "Microsoft.FSharp.Collections.Seq.head"
+            "Microsoft.FSharp.Collections.Seq.tryHead"
+            "Microsoft.FSharp.Collections.Seq.last"
+            "Microsoft.FSharp.Collections.Seq.tryLast"
+            "Microsoft.FSharp.Collections.Seq.exactlyOne"
+            "Microsoft.FSharp.Collections.Seq.tryExactlyOne"
+            "Microsoft.FSharp.Collections.Seq.indexed"
+            "Microsoft.FSharp.Collections.Seq.pairwise"
+            "Microsoft.FSharp.Collections.Seq.rev"
+            "Microsoft.FSharp.Collections.Seq.sort"
+            "Microsoft.FSharp.Collections.Seq.sortDescending"
+            "Microsoft.FSharp.Collections.Seq.sum"
+            "Microsoft.FSharp.Collections.Seq.tail"
+            "Microsoft.FSharp.Collections.Seq.averageBy"
+            "Microsoft.FSharp.Collections.Seq.append"
+            "Microsoft.FSharp.Collections.Seq.choose"
+            "Microsoft.FSharp.Collections.Seq.chunkBySize"
+            "Microsoft.FSharp.Collections.Seq.collect"
+            "Microsoft.FSharp.Collections.Seq.compareWith"
+            "Microsoft.FSharp.Collections.Seq.concat"
+            "Microsoft.FSharp.Collections.Seq.countBy"
+            "Microsoft.FSharp.Collections.Seq.distinctBy"
+            "Microsoft.FSharp.Collections.Seq.splitInto"
+            "Microsoft.FSharp.Collections.Seq.exists2"
+            "Microsoft.FSharp.Collections.Seq.where"
+            "Microsoft.FSharp.Collections.Seq.find"
+            "Microsoft.FSharp.Collections.Seq.findBack"
+            "Microsoft.FSharp.Collections.Seq.findIndex"
+            "Microsoft.FSharp.Collections.Seq.findIndexBack"
+            "Microsoft.FSharp.Collections.Seq.fold2"
+            "Microsoft.FSharp.Collections.Seq.foldBack2"
+            "Microsoft.FSharp.Collections.Seq.forall2"
+            "Microsoft.FSharp.Collections.Seq.groupBy"
+            "Microsoft.FSharp.Collections.Seq.item"
+            "Microsoft.FSharp.Collections.Seq.iteri"
+            "Microsoft.FSharp.Collections.Seq.iter2"
+            "Microsoft.FSharp.Collections.Seq.iteri2"
+            "Microsoft.FSharp.Collections.Seq.map2"
+            "Microsoft.FSharp.Collections.Seq.mapFold"
+            "Microsoft.FSharp.Collections.Seq.mapFoldBack"
+            "Microsoft.FSharp.Collections.Seq.map3"
+            "Microsoft.FSharp.Collections.Seq.mapi"
+            "Microsoft.FSharp.Collections.Seq.mapi2"
+            "Microsoft.FSharp.Collections.Seq.maxBy"
+            "Microsoft.FSharp.Collections.Seq.minBy"
+            "Microsoft.FSharp.Collections.Seq.permute"
+            "Microsoft.FSharp.Collections.Seq.pick"
+            "Microsoft.FSharp.Collections.Seq.reduce"
+            "Microsoft.FSharp.Collections.Seq.replicate"
+            "Microsoft.FSharp.Collections.Seq.reduceBack"
+            "Microsoft.FSharp.Collections.Seq.scan"
+            "Microsoft.FSharp.Collections.Seq.scanBack"
+            "Microsoft.FSharp.Collections.Seq.skip"
+            "Microsoft.FSharp.Collections.Seq.skipWhile"
+            "Microsoft.FSharp.Collections.Seq.sortWith"
+            "Microsoft.FSharp.Collections.Seq.sortBy"
+            "Microsoft.FSharp.Collections.Seq.sortByDescending"
+            "Microsoft.FSharp.Collections.Seq.sumBy"
+            "Microsoft.FSharp.Collections.Seq.take"
+            "Microsoft.FSharp.Collections.Seq.takeWhile"
+            "Microsoft.FSharp.Collections.Seq.tryFind"
+            "Microsoft.FSharp.Collections.Seq.tryFindBack"
+            "Microsoft.FSharp.Collections.Seq.tryFindIndex"
+            "Microsoft.FSharp.Collections.Seq.tryFindIndexBack"
+            "Microsoft.FSharp.Collections.Seq.tryItem"
+            "Microsoft.FSharp.Collections.Seq.tryPick"
+            "Microsoft.FSharp.Collections.Seq.transpose"
+            "Microsoft.FSharp.Collections.Seq.truncate"
+            "Microsoft.FSharp.Collections.Seq.unfold"
+            "Microsoft.FSharp.Collections.Seq.windowed"
+            "Microsoft.FSharp.Collections.Seq.zip"
+            "Microsoft.FSharp.Collections.Seq.zip3"
+            "Microsoft.FSharp.Collections.Seq.removeAt"
+            "Microsoft.FSharp.Collections.Seq.removeManyAt"
+            "Microsoft.FSharp.Collections.Seq.updateAt"
+            "Microsoft.FSharp.Collections.Seq.insertAt"
+            "Microsoft.FSharp.Collections.Seq.insertManyAt"
+            "Microsoft.FSharp.Collections.Seq.allPairs"
+        ]
+
+[<CliAnalyzer("VirtualCall Analyzer",
+              "Checks if calls of Seq functions can be replaced with functions from the collection modules")>]
 let virtualCallAnalyzer : Analyzer<CliContext> =
-    fun (context : CliContext) ->
+    fun (ctx : CliContext) ->
         async {
             let state = ResizeArray<string * string * range> ()
 
-            let seqFuncsWithEquivalentsInAllCollections =
-                set
-                    [
-                        "Microsoft.FSharp.Collections.Seq.isEmpty"
-                        "Microsoft.FSharp.Collections.Seq.length" // ~ Set.count
-                        "Microsoft.FSharp.Collections.Seq.max" // ~ Set.maxElement
-                        "Microsoft.FSharp.Collections.Seq.min" // ~ Set.minElement
-                        "Microsoft.FSharp.Collections.Seq.contains"
-                        "Microsoft.FSharp.Collections.Seq.exists"
-                        "Microsoft.FSharp.Collections.Seq.filter"
-                        "Microsoft.FSharp.Collections.Seq.foldBack"
-                        "Microsoft.FSharp.Collections.Seq.forall"
-                        "Microsoft.FSharp.Collections.Seq.iter"
-                        "Microsoft.FSharp.Collections.Seq.map"
-                        "Microsoft.FSharp.Collections.Seq.fold"
-                        "Microsoft.FSharp.Collections.Seq.except" // ~ Set.difference
-                    ]
-
-            let seqFuncsWithEquivalentsInArrayAndList =
-                set
-                    [
-                        "Microsoft.FSharp.Collections.Seq.average"
-                        "Microsoft.FSharp.Collections.Seq.distinct"
-                        "Microsoft.FSharp.Collections.Seq.head"
-                        "Microsoft.FSharp.Collections.Seq.tryHead"
-                        "Microsoft.FSharp.Collections.Seq.last"
-                        "Microsoft.FSharp.Collections.Seq.tryLast"
-                        "Microsoft.FSharp.Collections.Seq.exactlyOne"
-                        "Microsoft.FSharp.Collections.Seq.tryExactlyOne"
-                        "Microsoft.FSharp.Collections.Seq.indexed"
-                        "Microsoft.FSharp.Collections.Seq.pairwise"
-                        "Microsoft.FSharp.Collections.Seq.rev"
-                        "Microsoft.FSharp.Collections.Seq.sort"
-                        "Microsoft.FSharp.Collections.Seq.sortDescending"
-                        "Microsoft.FSharp.Collections.Seq.sum"
-                        "Microsoft.FSharp.Collections.Seq.tail"
-                        "Microsoft.FSharp.Collections.Seq.averageBy"
-                        "Microsoft.FSharp.Collections.Seq.append"
-                        "Microsoft.FSharp.Collections.Seq.choose"
-                        "Microsoft.FSharp.Collections.Seq.chunkBySize"
-                        "Microsoft.FSharp.Collections.Seq.collect"
-                        "Microsoft.FSharp.Collections.Seq.compareWith"
-                        "Microsoft.FSharp.Collections.Seq.concat"
-                        "Microsoft.FSharp.Collections.Seq.countBy"
-                        "Microsoft.FSharp.Collections.Seq.distinctBy"
-                        "Microsoft.FSharp.Collections.Seq.splitInto"
-                        "Microsoft.FSharp.Collections.Seq.exists2"
-                        "Microsoft.FSharp.Collections.Seq.where"
-                        "Microsoft.FSharp.Collections.Seq.find"
-                        "Microsoft.FSharp.Collections.Seq.findBack"
-                        "Microsoft.FSharp.Collections.Seq.findIndex"
-                        "Microsoft.FSharp.Collections.Seq.findIndexBack"
-                        "Microsoft.FSharp.Collections.Seq.fold2"
-                        "Microsoft.FSharp.Collections.Seq.foldBack2"
-                        "Microsoft.FSharp.Collections.Seq.forall2"
-                        "Microsoft.FSharp.Collections.Seq.groupBy"
-                        "Microsoft.FSharp.Collections.Seq.item"
-                        "Microsoft.FSharp.Collections.Seq.iteri"
-                        "Microsoft.FSharp.Collections.Seq.iter2"
-                        "Microsoft.FSharp.Collections.Seq.iteri2"
-                        "Microsoft.FSharp.Collections.Seq.map2"
-                        "Microsoft.FSharp.Collections.Seq.mapFold"
-                        "Microsoft.FSharp.Collections.Seq.mapFoldBack"
-                        "Microsoft.FSharp.Collections.Seq.map3"
-                        "Microsoft.FSharp.Collections.Seq.mapi"
-                        "Microsoft.FSharp.Collections.Seq.mapi2"
-                        "Microsoft.FSharp.Collections.Seq.maxBy"
-                        "Microsoft.FSharp.Collections.Seq.minBy"
-                        "Microsoft.FSharp.Collections.Seq.permute"
-                        "Microsoft.FSharp.Collections.Seq.pick"
-                        "Microsoft.FSharp.Collections.Seq.reduce"
-                        "Microsoft.FSharp.Collections.Seq.replicate"
-                        "Microsoft.FSharp.Collections.Seq.reduceBack"
-                        "Microsoft.FSharp.Collections.Seq.scan"
-                        "Microsoft.FSharp.Collections.Seq.scanBack"
-                        "Microsoft.FSharp.Collections.Seq.skip"
-                        "Microsoft.FSharp.Collections.Seq.skipWhile"
-                        "Microsoft.FSharp.Collections.Seq.sortWith"
-                        "Microsoft.FSharp.Collections.Seq.sortBy"
-                        "Microsoft.FSharp.Collections.Seq.sortByDescending"
-                        "Microsoft.FSharp.Collections.Seq.sumBy"
-                        "Microsoft.FSharp.Collections.Seq.take"
-                        "Microsoft.FSharp.Collections.Seq.takeWhile"
-                        "Microsoft.FSharp.Collections.Seq.tryFind"
-                        "Microsoft.FSharp.Collections.Seq.tryFindBack"
-                        "Microsoft.FSharp.Collections.Seq.tryFindIndex"
-                        "Microsoft.FSharp.Collections.Seq.tryFindIndexBack"
-                        "Microsoft.FSharp.Collections.Seq.tryItem"
-                        "Microsoft.FSharp.Collections.Seq.tryPick"
-                        "Microsoft.FSharp.Collections.Seq.transpose"
-                        "Microsoft.FSharp.Collections.Seq.truncate"
-                        "Microsoft.FSharp.Collections.Seq.unfold"
-                        "Microsoft.FSharp.Collections.Seq.windowed"
-                        "Microsoft.FSharp.Collections.Seq.zip"
-                        "Microsoft.FSharp.Collections.Seq.zip3"
-                        "Microsoft.FSharp.Collections.Seq.removeAt"
-                        "Microsoft.FSharp.Collections.Seq.removeManyAt"
-                        "Microsoft.FSharp.Collections.Seq.updateAt"
-                        "Microsoft.FSharp.Collections.Seq.insertAt"
-                        "Microsoft.FSharp.Collections.Seq.insertManyAt"
-                        "Microsoft.FSharp.Collections.Seq.allPairs"
-                    ]
 
             let walker =
                 { new TypedTreeCollectorBase() with
@@ -181,7 +183,7 @@ let virtualCallAnalyzer : Analyzer<CliContext> =
                                         state.Add (mfv.DisplayName, modules[0], range)
                 }
 
-            match context.TypedTree with
+            match ctx.TypedTree with
             | Some t -> walkTast walker t
             | None -> ()
 
