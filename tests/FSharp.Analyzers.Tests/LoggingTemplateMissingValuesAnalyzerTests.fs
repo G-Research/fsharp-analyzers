995bca01b4cda87d925e209e2adf540620c07cec
dawe <dawedawe@posteo.de>
2024-01-10 12:36:07 +0100

    add the LoggingTemplateMissingValuesAnalyzer (#53)
    * add the LoggingTemplateMissingValuesAnalyzer
    
    * - show discrete name of used ILogger method
    - add tests for explicit value array
    
    * release 0.7.0

---
+++ tests/FSharp.Analyzers.Tests/LoggingTemplateMissingValuesAnalyzerTests.fs
@@ -0,0 +1,71 @@
+namespace GR.FSharp.Analyzers.Tests
+
+module LoggingTemplateMissingValuesAnalyzerTests =
+
+    open System.Collections
+    open System.IO
+    open NUnit.Framework
+    open FSharp.Compiler.CodeAnalysis
+    open FSharp.Analyzers.SDK.Testing
+    open GR.FSharp.Analyzers
+    open GR.FSharp.Analyzers.Tests.Common
+
+    let mutable projectOptions : FSharpProjectOptions = FSharpProjectOptions.zero
+
+    [<SetUp>]
+    let Setup () =
+        task {
+            let! options =
+                mkOptionsFromProject
+                    "net7.0"
+                    [
+                        {
+                            Name = "Microsoft.Extensions.Logging"
+                            Version = "8.0.0"
+                        }
+                        {
+                            Name = "Microsoft.Extensions.Logging.Console"
+                            Version = "8.0.0"
+                        }
+                    ]
+
+            projectOptions <- options
+        }
+
+    type TestCases() =
+
+        interface IEnumerable with
+            member _.GetEnumerator () : IEnumerator =
+                constructTestCaseEnumerator [| "loggingtemplatemissingvalues" |]
+
+    [<TestCaseSource(typeof<TestCases>)>]
+    let Tests (fileName : string) =
+        task {
+            let fileName = Path.Combine (dataFolder, fileName)
+
+            let! messages =
+                File.ReadAllText fileName
+                |> getContext projectOptions
+                |> LoggingTemplateMissingValuesAnalyzer.loggingTemplateMissingValuesCliAnalyzer
+
+            do! assertExpected fileName messages
+        }
+
+    type NegativeTestCases() =
+
+        interface IEnumerable with
+            member _.GetEnumerator () : IEnumerator =
+                constructTestCaseEnumerator [| "loggingtemplatemissingvalues" ; "negative" |]
+
+    [<TestCaseSource(typeof<NegativeTestCases>)>]
+    let NegativeTests (fileName : string) =
+        task {
+            let fileName = Path.Combine (dataFolder, fileName)
+
+            let! messages =
+                File.ReadAllText fileName
+                |> getContext projectOptions
+                |> LoggingTemplateMissingValuesAnalyzer.loggingTemplateMissingValuesCliAnalyzer
+
+            Assert.That (messages, Is.Empty)
+        }
