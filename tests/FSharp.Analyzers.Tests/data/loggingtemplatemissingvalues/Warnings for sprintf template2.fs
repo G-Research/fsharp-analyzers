5cca24709d983c253035d6cda276f7922113e53e
dawe <dawedawe@posteo.de>
2024-02-16 13:24:17 +0100

    detect template strings constructed by functions like sprintf

---
+++ tests/FSharp.Analyzers.Tests/data/loggingtemplatemissingvalues/Warnings for sprintf template2.fs
@@ -0,0 +1,10 @@
+module M
+
+    open Microsoft.Extensions.Logging
+
+    let testlog () =
+        use factory = LoggerFactory.Create(fun b -> b.AddConsole() |> ignore)
+        let logger: ILogger = factory.CreateLogger("Program")
+        let timeWasteString = "time waste"
+
+        logger.LogWarning(sprintf "Foo {Wasted}\n{Result}", timeWasteString)
---
+++ tests/FSharp.Analyzers.Tests/data/loggingtemplatemissingvalues/negative/No warnings for sprintf template2.fs
@@ -0,0 +1,12 @@
+module M
+
+    open Microsoft.Extensions.Logging
+
+    let testlog () =
+        use factory = LoggerFactory.Create(fun b -> b.AddConsole() |> ignore)
+        let logger: ILogger = factory.CreateLogger("Program")
+        let timespan = System.TimeSpan()
+        let timeWasteString = "time waste"
+        let result = "result"
+
+        logger.LogWarning(sprintf "Foo {Wasted}\n{Result}", timeWasteString, result)
